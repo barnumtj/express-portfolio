13363ac62f67ca979d2a23acce65b9b45d178c5d
barnumtj <tanner.barnum@icloud.com>
2018-09-25 22:10:46 -0500

    adding projects

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var path = require("path");
+
+app.use(express.static('public'))
+
+app.get('/',function(req,res){     
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/portfolio', function(req, res){
+    res.sendFile(path.join(__dirname+'/views/portfolio.html'))
+})
+app.listen(PORT)
